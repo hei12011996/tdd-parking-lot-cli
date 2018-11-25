b1d33a4fcde4b7c98c883c49eeb338d22f3351c3
Antony Chan <antony.chan@oocl.com>
2018-11-25 23:57:38 +0800

    Implement SmartParkingBoy

--- src/main/java/com/oocl/cultivation/SmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SmartParkingBoy.java
@@ -1,16 +1,38 @@
 package com.oocl.cultivation;
 
-public class SmartParkingBoy {
+public class SmartParkingBoy extends ParkingPerson {
 
     public SmartParkingBoy(ParkingLot... parkingLots) {
-
+        super(parkingLots);
     }
 
+    @Override
     public ParkingTicket park(Car car) {
-        return null;
+        ParkingLot availableParkingLot = findParkingLotThatContainsMostEmptyPositions();
+        ParkingTicket ticket = parkCarToParkingLot(car, availableParkingLot);
+        return ticket;
     }
 
+    @Override
     public Car fetch(ParkingTicket ticket) {
-        return null;
+        Car car = null;
+        if (isTicketProvided(ticket)){
+            car = getCarFromParkingLot(ticket);
+        } else {
+            setLastErrorMessage(lastErrorMessage = "Please provide your parking ticket.");
+        }
+        return car;
+    }
+
+    private ParkingLot findParkingLotThatContainsMostEmptyPositions(){
+        ParkingLot mostEmptyPositionsParkingLot = null;
+        int maxNumberOfEmptyPositions = 0;
+        for (ParkingLot parkingLot : this.parkingLots){
+            if (parkingLot.getAvailableParkingPosition() > maxNumberOfEmptyPositions) {
+                maxNumberOfEmptyPositions = parkingLot.getAvailableParkingPosition();
+                mostEmptyPositionsParkingLot = parkingLot;
+            }
+        }
+        return mostEmptyPositionsParkingLot;
     }
 }
