e4edde441ed6421b9df9bd90f4eb46d39075d9d6
Antony Chan <antony.chan@oocl.com>
2018-11-25 23:38:27 +0800

    enhance ParkingBoy to support multiple parking lots

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,52 +1,85 @@
 package com.oocl.cultivation;
 
+import java.util.*;
+
 public class ParkingBoy {
 
-    private final ParkingLot parkingLot;
     private String lastErrorMessage;
+    protected final List<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
+    protected final Map<ParkingTicket, ParkingLot> parkingLotStorage = new HashMap<ParkingTicket, ParkingLot>();
 
-    public ParkingBoy(ParkingLot parkingLot) {
-        this.parkingLot = parkingLot;
+    public ParkingBoy(ParkingLot... parkingLots) {
+        this.parkingLots.addAll(Arrays.asList(parkingLots));
     }
 
     public ParkingTicket park(Car car) {
-        if (this.parkingLot.getAvailableParkingPosition() == 0){
-            this.lastErrorMessage = "The parking lot is full.";
-            return null;
-        } else {
-            clearLastErrorMessage();
-            return this.parkingLot.parkCar(car);
-        }
+        ParkingLot availableParkingLot = findAvailableParkingLot();
+        ParkingTicket ticket = parkCarToParkingLot(car, availableParkingLot);
+        return ticket;
     }
 
     public Car fetch(ParkingTicket ticket) {
-        if (isTicketValid(ticket)){
-            return getCarFromParkingLot(ticket);
+        Car car = null;
+        if (isTicketProvided(ticket)){
+            car = getCarFromParkingLot(ticket);
         } else {
-            this.lastErrorMessage = "Please provide your parking ticket.";
-            return null;
+            setLastErrorMessage(lastErrorMessage = "Please provide your parking ticket.");
         }
+        return car;
     }
 
     public String getLastErrorMessage() {
         return lastErrorMessage;
     }
 
-    private boolean isTicketValid(ParkingTicket ticket){
-        return ticket != null;
+    private void setLastErrorMessage(String errorMsg){
+        this.lastErrorMessage = errorMsg;
+    }
+
+    private ParkingTicket parkCarToParkingLot(Car car, ParkingLot parkingLot){
+        if (parkingLot == null){
+            setLastErrorMessage("The parking lot is full.");
+            return null;
+        } else {
+            clearLastErrorMessage();
+            ParkingTicket ticket = parkingLot.parkCar(car);
+            this.parkingLotStorage.put(ticket, parkingLot);
+            return ticket;
+        }
     }
 
     private Car getCarFromParkingLot(ParkingTicket ticket){
-        Car fetchedCar = this.parkingLot.returnCar(ticket);
-        if (fetchedCar == null) {
-            this.lastErrorMessage = "Unrecognized parking ticket.";
+        ParkingLot parkingLot = findParkingLotByTicket(ticket);
+        if (parkingLot == null) {
+            setLastErrorMessage("Unrecognized parking ticket.");
+            return null;
         } else {
             clearLastErrorMessage();
+            this.parkingLotStorage.remove(ticket);
         }
-        return fetchedCar;
+        return parkingLot.returnCar(ticket);
+    }
+
+    private boolean isTicketProvided(ParkingTicket ticket){
+        return ticket != null;
     }
 
     private void clearLastErrorMessage() {
         this.lastErrorMessage = null;
+    }
+
+    private ParkingLot findParkingLotByTicket(ParkingTicket ticket){
+        return parkingLotStorage.get(ticket);
+    }
+
+    private ParkingLot findAvailableParkingLot(){
+        ParkingLot availableParkingLot = null;
+        for (ParkingLot parkingLot : this.parkingLots){
+            if (parkingLot.getAvailableParkingPosition() > 0) {
+                availableParkingLot = parkingLot;
+                break;
+            }
+        }
+        return availableParkingLot;
     }
 }
