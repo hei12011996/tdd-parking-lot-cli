b22a5d4f93aad0bd5e6c35a9847e06383660a48f
Antony Chan <antony.chan@oocl.com>
2018-11-26 02:48:18 +0800

    implement ParkingManager

---
+++ src/main/java/com/oocl/cultivation/ParkingManager.java
@@ -0,0 +1,81 @@
+package com.oocl.cultivation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ParkingManager extends ParkingBoy {
+
+    private final List<ParkingBoy> employees = new ArrayList<ParkingBoy>();
+    private String lastErrorMessageFromParkingBoy;
+
+    public ParkingManager(ParkingLot... parkingLots) {
+        super(parkingLots);
+    }
+
+    @Override
+    public ParkingTicket park(Car car) {
+        ParkingLot availableParkingLot = findAvailableParkingLot();
+        ParkingTicket ticket = parkCarToParkingLot(car, availableParkingLot);
+        return ticket;
+    }
+
+    public ParkingTicket tellParkingBoyToPark(ParkingBoy parkingBoy, Car car) {
+        if (!isParkingBoyUnderManagement(parkingBoy)) {
+            setLastErrorMessage("That parking boy is not under management.");
+            return null;
+        } else {
+            ParkingTicket ticket = parkingBoy.park(car);
+            setLastErrorMessageFromParkingBoy(parkingBoy.getLastErrorMessage());
+            return ticket;
+        }
+    }
+
+    @Override
+    public Car fetch(ParkingTicket ticket) {
+        Car car = null;
+        if (isTicketProvided(ticket)){
+            car = getCarFromParkingLot(ticket);
+        } else {
+            setLastErrorMessage("Please provide your parking ticket.");
+        }
+        return car;
+    }
+
+    public Car tellParkingBoyToFetch(ParkingBoy parkingBoy, ParkingTicket ticket) {
+        if (!isParkingBoyUnderManagement(parkingBoy)) {
+            setLastErrorMessage("That parking boy is not under management.");
+            return null;
+        } else {
+            Car car = parkingBoy.fetch(ticket);
+            setLastErrorMessageFromParkingBoy(parkingBoy.getLastErrorMessage());
+            return car;
+        }
+    }
+
+    public String getLastErrorMessageFromParkingBoy() {
+        return lastErrorMessageFromParkingBoy;
+    }
+
+    protected void setLastErrorMessageFromParkingBoy(String errorMsg){
+        this.lastErrorMessageFromParkingBoy = errorMsg;
+    }
+
+    public void manage(ParkingBoy parkingBoy){
+        employees.add(parkingBoy);
+    }
+
+    private ParkingLot findAvailableParkingLot(){
+        ParkingLot availableParkingLot = null;
+        for (ParkingLot parkingLot : this.parkingLots){
+            if (parkingLot.getAvailableParkingPosition() > 0) {
+                availableParkingLot = parkingLot;
+                break;
+            }
+        }
+        return availableParkingLot;
+    }
+
+    private boolean isParkingBoyUnderManagement(ParkingBoy parkingBoy){
+        return this.employees.contains(parkingBoy);
+    }
+}
